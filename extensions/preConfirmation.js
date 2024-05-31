b2a0d71409a60c2a10034884df1c6f2f30c4209f
Rex <kniveslikebutterflies@gmail.com>
2024-05-30 20:11:05 -0400

    removing comments

--- extensions/preConfirmation.js
+++ extensions/preConfirmation.js
@@ -3,53 +3,8 @@ const { Events } = require('discord.js');
 const logger = require('../utilities/logger');
 const _ = require('lodash');
 
-/*
-    First piece of Middleware to really test and flesh out the
-    idea. This one will:
-    - [x] receive an outgoing message
-    - [x] generate an async representation of the work for storage
-    - [x] generate and send a confirmation button interaction in it's place
-    - [x] setup an interaction listener for button press
-    - [x] retrieve stored work
-    - [x] run, parse, or otherwise process it
-
-*/
-
 const genConfInst = (dc, approvalUser) => {
-/*
-    const theFunc = (inMsg, outMsg, next) => {
 
-        // Using func; the DM -> Channel Msg flow is too disparate to use msg ids
-        CallStateMap.inst.set( theFunc, () => {
-
-            discordClient.channels
-                .fetch(inMsg.channel_id)
-                .then((chan) => chan.send(inMsg))
-                .then(message => logger.log(` message successfully: ${message.content}`))
-                .catch(logger.error);
-        } );
-
-        discordClient.on(Events.MessageCreate, (msg) => {
-
-            if (msg.author.id === approvalUser.id) {
-                if(msg.startsWith('1')) {
-                    CallStateMap.inst.get(theFunc)?.();
-                } else if(msg.startsWith('2')) {
-                    CallStateMap.inst.delete(theFunc);
-                }
-            }
-        });
-
-        const dmChannel = approvalUser
-            .createDM()
-            .then((userDmChannel) => userDmChannel.send('The Bot would like approval to send the following message: /n ' + inMsg.content + ' /n reply with: /n 1) yes /n 2) no'))
-            .then(message => logger.log(`Sent message successfully: ${message.content}`))
-            .catch(logger.error);
-    };
-
-    return theFunc;
-}
-*/
     return (inMsg, outMsg, next) => {
 
         const approvalText = 'Hello \n' +
