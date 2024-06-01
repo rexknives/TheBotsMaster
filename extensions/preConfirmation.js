cce65b2bff31bcb0badd11a99998499354f592f9
Rex <kniveslikebutterflies@gmail.com>
2024-06-01 11:45:50 -0400

    event handler cleanup

--- extensions/preConfirmation.js
+++ extensions/preConfirmation.js
@@ -7,23 +7,45 @@ const genConfInst = (dc, approvalUser) => {
 
     return (inMsg, outMsg, next) => {
 
+        // TODO: convert to service agnostic version
+
         const approvalText = 'Hello \n' +
             'The Bot would like approval to send the following message:' +
             `\n ${inMsg.content} \n reply with: \n 1) yes \n 2) no`;
 
+        const msgProc = (msg, resolve, reject) => {
+            if (msg.author.id !== approvalUser.id) return;
+            msg?.content?.startsWith?.('1') ? resolve() : reject();
+        };
+        
+        let partialMsgProc;
+
         approvalUser.createDM()
             .then( usrDMC => usrDMC.send(approvalText) )
             .then( msg => logger.log(`Sent message successfully: ${msg}`) )
-            .then( () => new Promise((rs, rj) => dc.on(Events.MessageCreate, _.partial((msg, s, r) => {
-                if (msg.author.id !== approvalUser.id) return;
-                msg?.content?.startsWith?.('1') ? s() : r();
-            }, _, rs, rj))) )
+            .then( () => new Promise((rs, rj) => {
+                partialMsgProc = _.partial(msgProc, _, rs, rj);
+                dc.on(Events.MessageCreate, partialMsgProc);
+            } ))
             .then(() => dc.channels.fetch(inMsg.channel_id) )
             .then( chan => { chan.send(inMsg) } )
             .then( msg => logger.log(`message successfully: ${msg}`) )
+            .finally( () => dc.removeListener(Events.MessageCreate, partialMsgProc) )
             .catch( logger.error );
 
         next(outMsg, 'some other dispatched work or state here?');
+
+        /* Service agnostic...
+
+            approvalUser.sendPrivMsg(approvalText) )
+                .then( () => new Promise((rs, rj) => dcs.on(ChatEvents.ChannelMessage, _.partial((msg, s, r) => {
+                    if (msg.author.id !== approvalUser.id) return;
+                    msg?.content?.startsWith?.('1') ? s() : r();
+                }, _, rs, rj))) )
+                .then( () => { chan.send(inMsg) } )
+                .finally( () => { dcsre.removeListener(ChatEvents.ChannelMessage, ...) } );
+
+        */ 
     }
 }
 
