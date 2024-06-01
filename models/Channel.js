43f5ff43e6da54b9d53468ce04b7e3939a9e50ab
Rex <kniveslikebutterflies@gmail.com>
2024-06-01 11:46:05 -0400

    Channel class method sketches

---
+++ models/Channel.js
@@ -0,0 +1,18 @@
+module.exports = class Channel {
+
+    constructor() {
+
+    }
+    
+    leave = () => {
+
+    }
+
+    getUsers = () => {
+        
+    }
+
+    static channelFactory = (discordJsChannel) => {
+
+    }
+}
