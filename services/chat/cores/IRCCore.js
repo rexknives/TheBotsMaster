1434ce0bc339e2bb9cb071a30398e343668e67eb
Rex <kniveslikebutterflies@gmail.com>
2024-06-02 14:34:11 -0400

    working pass at IRCCore

--- services/chat/cores/IRCCore.js
+++ services/chat/cores/IRCCore.js
@@ -1,54 +1,57 @@
-import irc from 'matrix-org-irc';
+const irc = require('matrix-org-irc');
+const logger = require('../../../utilities/logger');
+const ChatService = require('../../../models/ChatService');
 
 module.exports = class IRCCore extends ChatService {
 
-    constructor(opts) {
-
-        this.options = opts;
-        this.client = new irc.Client(opts);
+    constructor(...args) {
+        super(...args);
+        this.options = args;
+        this.client = new irc.Client(...this.options);
     }
 
-    setupListeners = () => {
+    login = () => this.client.connect();
 
-        this.client.addListener('error', (message) => {
-            console.error('ERROR: %s: %s', message.command, message.args.join(' '));
+    logout = () => {
+
+        this.client.disconnect();
+
+        this.client.eventNames().forEach((evt) => {
+            this.client.removeAllListeners(evt);
         });
 
-        this.client.addListener('message#blah', (from, message) => {
-            console.log('<%s> %s', from, message);
-        });
-        
-        this.client.addListener('message', (from, to, message) => {
-            console.log('%s => %s: %s', from, to, message);
-        
-            if (to.match(/^[#&]/)) {
-                // channel message
-                if (message.match(/hello/i)) {
-                    this.client.say(to, 'Hello there ' + from);
-                }
-                if (message.match(/dance/)) {
-                    setTimeout(() => { this.client.say(to, '\u0001ACTION dances: :D\\-<\u0001'); }, 1000);
-                    setTimeout(() => { this.client.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 2000);
-                    setTimeout(() => { this.client.say(to, '\u0001ACTION dances: :D/-<\u0001');  }, 3000);
-                    setTimeout(() => { this.client.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 4000);
-                }
-            }
-            else {
-                // private message
-                console.log('private message');
-            }
-        });
-        this.client.addListener('pm', (nick, message) => {
-            console.log('Got private message from %s: %s', nick, message);
-        });
-        this.client.addListener('join', (channel, who) => {
-            console.log('%s has joined %s', who, channel);
-        });
-        this.client.addListener('part', (channel, who, reason) => {
-            console.log('%s has left %s: %s', who, channel, reason);
-        });
-        this.client.addListener('kick', (channel, who, by, reason) => {
-            console.log('%s was kicked from %s by %s: %s', who, channel, by, reason);
+        this.client.destroy();
+        return this.client;
+    }
+
+    join = (channelName, cb) => this.client.join(channelName, cb);
+
+    leave = (channelName, cb) => this.client.part(channelName, cb);
+
+    listChannels = (...args) => this.client.list(...args);
+
+    sendMsg = (...args) => this.client.say(...args);
+
+    sendPrivMsg = (...args) => this.client.say(...args);
+
+    on = (...args) => this.client?.on(...args);
+
+    once = (...args) => this.client?.once(...args);
+
+    onceFilter = (...args) => ChatEvent.onceFilter(this.client, ...args);
+
+    getWaterhoseFeed = (eventProcessor, off = false, events = IRCCore.allEvents()) => {
+        events.forEach((evt)=>{
+            logger.debug(`adding ${evt} to waterhose event listener.`);
+            this.client[off ? 'removeListener' : 'on'](evt, eventProcessor);
         });
     }
+
+    static allEvents = () => [
+        'registered', 'notice', 'mode_is', '+mode', '-mode', 'nick', 'motd', 'action', 'ctcp',
+        'raw', 'kick', 'names', 'topic', 'channellist', 'channellist_start', 'channellist_item',
+        'whois', 'selfMessage', 'kill', 'message', 'pm', 'invite', 'quit', 'join', 'abort',
+        'connect', 'error', 'sasl_error', 'sasl_loggedin', 'sasl_loggedout', 'ping', 'pong',
+        'netError', 'part', 'isupport'
+    ];
 }
