cf25b0490ac8e0a2bdacd0fd4f80b3347a75f206
Rex <kniveslikebutterflies@gmail.com>
2024-06-03 02:06:29 -0400

    first pass at telephony

--- services/chat/cores/TelephonyCore.js
+++ services/chat/cores/TelephonyCore.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const twilio = require('twilio');
+const ChatService = require('../../../models/ChatService');
 const logger = require('../../../utilities/logger');
 
-module.exports = class TelephonyCore {
+module.exports = class TelephonyCore extends ChatService {
 
     constructor(opts, ...args) {
 
         this.options = opts;
+        this.client = new twilio(opts.accountSid, opts.authToken, {logLevel: 'debug'});
     }
 
     login = () => {
@@ -18,19 +20,24 @@ module.exports = class TelephonyCore {
     setupVoiceHTTPServer = () => {
         this.httpServer = express();
         this.options.port = this.options.port || 3000;
-        this.httpServer.get('/twiml', this.twimlEndpoint);
-
+        this.registerEndpoint('/twiml', this.getAudioFilePlayEndpoint());
+        this.registerEndpoint('/incoming', this.getAudioFilePlayEndpoint(), 'post');
         this.httpServer.listen(this.options.port, () => {
             console.log(`Server is listening on port ${port}`);
         });
     }
 
-    twimlEndpoint = (req, res) => {
-        const twiml = new twilio.twiml.VoiceResponse();
-        twiml.play('https://www.example.com/audio-file.mp3');
-        
-        res.type('text/xml');
-        res.send(twiml.toString());
+    registerEndpoint = (endpointURL, requestHandler, verb = 'get') => {
+        this.httpServer?.[verb]?.(endpointURL, requestHandler);
+    }
+
+    getAudioFilePlayEndpoint = (audioFileURL = 'https://www.example.com/audio-file.mp3') => {
+        return (req, res) => {
+            const twiml = new twilio.twiml.VoiceResponse();
+            twiml.play(audioFileURL);
+            res.type('text/xml');
+            res.send(twiml.toString());
+        }
     }
 
     logout = () => {}
@@ -43,9 +50,43 @@ module.exports = class TelephonyCore {
     
     sendMsg = (channelOrName, msg) => {};
 
-    sendPrivMsg = (user, privMsg) => {}
+    sendPrivMsg = (toUserOrNumber, toUserOrNumber, privMsg, ) => {
+        return this.client.messages.create({
+            body: privMsg,
+            from: toUserOrNumber,
+            to: toUserOrNumber
+        })
+        .then(logger.log)
+        .catch(logger.error);
+    }
+
+    setupSMShandler = () => {
+        this.registerEndpoint('/sms', (req, res) => {
+            const twiml = new twilio.twiml.MessagingResponse();
+          
+            // Process the incoming message
+            const incomingMessage = req.body.Body;
+            logger.debug('Received message:', incomingMessage);
+          
+            // Respond with a confirmation message
+            twiml.message('Your message has been received.');
+          
+            res.type('text/xml');
+            res.send(twiml.toString());
+        }, 'post');
+    }
 
     send = (msgObj) => {}
+
+    startVoiceCall = (toUserOrNumber, fromUserOrNumber, apiUrl, cb) => {
+        return this.client.calls.create({
+            to: userOrNumber,
+            from: fromUserOrNumber,
+            url: `${apiUrl}:${this.options.port}`
+        })
+        .then(logger.log)
+        .catch(logger.error);
+    }
 
     setMetaData = setMetaData.bind(this);
 
