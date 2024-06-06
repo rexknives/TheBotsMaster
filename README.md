4b0c3baa9c0d9e83ef7eb2d9c982cac2a0089122
Rex <kniveslikebutterflies@gmail.com>
2024-06-06 04:07:29 -0400

    meta

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ Chat bot framework supporting numerous chat services and composition flexibility
 - one bot, one service, serveral different usernames/accounts, several channels per each
 - several "bots" (conceptually speaking), premade, tied together into one with very little code
 
+Example Bots
+- [ ] VoiceMail Bot (encrypted chat text, chat voice, phone voice, and phone SMS storage)
+- [ ] Operator Bot (telephony a la The Americans)
+- [ ] 
+
 # Todo:
 - [ ] Events
     - [ ] automatically set up msg and privMsg event listeners to dispatch "synthetic" Events
@@ -15,8 +20,12 @@ Chat bot framework supporting numerous chat services and composition flexibility
     - [ ] wire msgs into Middleware stack after event listeners
 - [ ] API for voice chat
 - [ ] Assistant Server/API - turns bot into API
-- [ ] Normalize Role mgmt subsystem
-- [ ] Normalize Sharding subsystem & API
+- [ ] Polyfills
+    - [ ] Reply & Threading subsytems & polyfills
+    - [ ] media attachment subsystem & polyfill
+    - [ ] Voice & video chat polyfill
+    - [ ] Normalize Role mgmt subsystem
+    - [ ] Normalize Sharding subsystem & API
 - [ ] caches
     - [ ] users
     - [ ] channels
--- scrap.js
+++ scrap.js
@@ -1,44 +1,80 @@
-const DISCORD_BOT_TOKEN = require('./config.js');
+const { DISCORD_BOT_TOKEN } = require('./config.js');
 const { Client, Events, GatewayIntentBits, Intents, TextChannel } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const DiscordCore = require('./services/chat/cores/DiscordCore.js');
 const IRCCore = require('./services/chat/cores/IRCCore.js');
 
-//const client = new DiscordCore({ intents: Object.values(GatewayIntentBits) });
 
+const ChatEvent = require('./models/ChatEvent.js');
+/*
 const client = new IRCCore('irc.libera.chat', 'SubwayBot', {
     debug: true,
     channels: ['#rexTestingStuff']
 });
+*/
+const dcClient = new DiscordCore({ intents: Object.values(GatewayIntentBits) });
+global.coreClient = dcClient;
 
-global.coreClient = client;
-
+/*
 client.on('motd', (...args) => console.log(...args));
 
 client.on('error', (...args) => console.log(...args));
 
-client.getWaterhoseFeed( (...args) => console.log(...args) );
+dcClient.getWaterhoseFeed( (...args) => console.log(...args) );
+*/
 
+
+
+dcClient.on( ChatEvent.events.LOGIN_EVENT, (evt) => {
+
+    console.log(`Ready! Logged in as ${evt.rawClient?.user?.id}`);
+
+    evt.client._initListeners();
+
+    evt.client.listChannels()
+        .then(console.log);
 /*
-coreClient.on(Events.ClientReady, (client) => {
-    console.log(`Ready! Logged in as ${client.user.id}`);
-
-    client.channels.fetch('1230593583963312148')
+    cl.channels.fetch('1230593583963312148')
     .then(chan => { 
         //console.log(chan);
         //global.coreClient.sendMsg(chan, "testing DiscordCore.js method sendMsg")
     });
 
-    client.users.fetch('721912593383686144')
+    cl.users.fetch('721912593383686144')
     .then(user => { 
         //console.log(user);
         global.coreClient.sendPrivMsg(user, "testing DiscordCore.js method sendPrivMsg")
     });
-
+*/
     //global.coreClient.join('1230593583963312148', (evt) => console.log(evt));
-    
 });
 
-global.coreClient.login(DISCORD_BOT_TOKEN);
+
+global.coreClient.login(DISCORD_BOT_TOKEN)
+    .then((loginEvent) => {
+        
+        console.log(`Ready! Logged in as ${loginEvent.rawClient?.user?.id}`);
+
+        //loginEvent.client._initListeners();
+
+        loginEvent.client.listChannels()
+            .then(console.log)
+            .catch(console.error);
+    });
+
+/*
+const rest = new REST({ version: '9' }).setToken(DISCORD_BOT_TOKEN);
+
+(async () => {
+  try {
+    const response = await rest.get(
+      Routes.guildChannels('1125441170168881182')
+    );
+
+    console.log('Channels:', JSON.stringify(response));
+  } catch (error) {
+    console.error('Error fetching channels:', error);
+  }
+})();
 */
-
-
